bcc387eb2d8504942eeda784890f63e2460bf51a
Urbain Corentin <corentin.urbain96@gmail.com>
2019-08-23 01:01:17 +0200

    Add XML parser and prepare JSON conversion

---
+++ StarCitizen XML to JSON/SCType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StarCitizen_XML_to_JSON
+{
+	public enum SCType
+	{
+		None	= 0b000, // 0
+		Ship	= 0b001, // 1
+		Weapon	= 0b010, // 2
+		Every	= 0b111, // 7
+	}
+}
