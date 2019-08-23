eea8f97ec8872f63613f0d969f8f918ecef8857a
Urbain Corentin <corentin.urbain96@gmail.com>
2019-08-23 18:08:38 +0200

    JShip: get all ships models

--- StarCitizen XML to JSON/JsonObjects/Ship/JShip.cs
+++ StarCitizen XML to JSON/JsonObjects/Ship/JShip.cs
@@ -16,49 +16,71 @@ namespace StarCitizen_XML_to_JSON.JsonObjects.Ship
 			if (!Directory.Exists(Path.Combine(destination, directory_name)))
 				Directory.CreateDirectory(Path.Combine(destination, directory_name));
 
-			this.filename = Path.Combine(destination, directory_name, file.Name.Replace(file.Extension, null) + ".json");
+			//this.filename = Path.Combine(destination, directory_name, file.Name.Replace(file.Extension, null) + ".json");
 		}
 
 		public override void Process()
 		{
+			var root = doc.SelectSingleNode("/*");
+
+			var models = GetShipsModels(root); // Get all ships models XML
+			foreach (var m in models)
+			{
+				if(m.model != null)
+					WriteFile(m.model);
+			}
+
+		}
+
+		private JShipModification[] GetShipsModels(XmlNode root)
+		{
+			List<JShipModification> modifications = new List<JShipModification>();
+
+			// check if has modifications
+			if (root.SelectSingleNode("/Vehicle/Modifications") == null)
+				return new JShipModification[0];
+
+			// Load all modifications
+			foreach (XmlNode node in root.SelectSingleNode("/Vehicle/Modifications").ChildNodes)
+			{
+				var n_name = node.Attributes["name"]?.Value ?? null;
+				var n_patchFile = node.Attributes["patchFile"]?.Value ?? null;
+				if (n_patchFile != null)
+					n_patchFile = Path.Combine(file.DirectoryName, n_patchFile + ".xml");
+
+				if (n_name == null)
+					continue;
+				// start replacement of modification
+				modifications.Add(new JShipModification(n_name, n_patchFile, node));
+			}
+
+			return modifications.ToArray();
+		}
+
+		private void WriteFile(XmlDocument doc)
+		{
+			XmlNode root = doc.FirstChild;
+
+			var name = (root.Attributes["displayname"] ?? root.Attributes["name"]).Value;
+
 			StringBuilder sb = new StringBuilder();
 			StringWriter sw = new StringWriter(sb);
 			using (JsonWriter writer = new JsonTextWriter(sw))
 			{
-				var root = doc.SelectSingleNode("/*");
-
 				writer.WriteStartObject();
 
 				writer.WritePropertyName("displayname");
 
-				var name = Sanitize((root.Attributes["displayname"] ?? root.Attributes["name"]).Value);
-				writer.WriteValue(name);
+				writer.WriteValue(Sanitize(name));
 
-				List<JShipModification> modifications = new List<JShipModification>();
-				foreach (XmlNode node in root.SelectSingleNode("/Vehicle/Modifications").ChildNodes)
-				{
-					var n_name = node.Attributes["name"]?.Value ?? null;
-					var n_patchFile = node.Attributes["patchFile"]?.Value ?? null;
-					if (n_patchFile != null)
-						n_patchFile = Path.Combine(file.DirectoryName, n_patchFile + ".xml");
-
-					modifications.Add(new JShipModification(n_name, n_patchFile , node));
-				}
 
 				writer.WriteEndObject();
 			}
 
-
-			using (StreamWriter writer = new StreamWriter(filename))
+			using (StreamWriter writer = new StreamWriter(Path.Combine(base.destination, directory_name, name + ".json")))
 			{
 				writer.Write(sb.ToString());
 			}
-			//Environment.Exit(0);
-		}
-
-		private void LoadModifications()
-		{
-
 		}
 	}
 }
