1e9f8cf2f5c458bf5a63c573aef8307fbfef321c
Urbain Corentin <corentin.urbain96@gmail.com>
2019-08-24 18:20:08 +0200

    JObject: Add ValidateFiles() method

--- StarCitizen XML to JSON/JObject.cs
+++ StarCitizen XML to JSON/JObject.cs
@@ -10,22 +10,40 @@ namespace StarCitizen_XML_to_JSON
 	{
 		public XmlDocument doc { get; private protected set; } = null;
 		public FileInfo file;
-		public string filename;
-		public string source;
+		internal List<FileInfo> generatedFiles;
+
 		public string destination;
 
 		public string directory_name = null;
 
+	
 		protected JObject(XmlDocument doc, FileInfo file, string destination)
 		{
 			this.doc = doc ?? throw new ArgumentNullException(nameof(doc));
 			this.file = file ?? throw new ArgumentNullException(nameof(file));
 			this.destination = destination ?? throw new ArgumentNullException(nameof(destination));
 
+			this.generatedFiles = new List<FileInfo>();
 			doc.Load(file.FullName);
 		}
 
 		public abstract void Process();
+		/// <summary>
+		/// Valid all generated files by loading them
+		/// </summary>
+		public void ValidateFiles()
+		{
+				if (generatedFiles.Count == 0)
+					throw new Exception("The variable 'generatedFiles' can't empty");
+
+				foreach (var file in generatedFiles)
+				{
+					using (StreamReader reader = new StreamReader(file.FullName))
+					{
+						Newtonsoft.Json.JsonConvert.DeserializeObject(reader.ReadToEnd());
+					}
+				}
+		}
 
 		public string Sanitize(string value)
 		{
