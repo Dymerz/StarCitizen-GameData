04e4661b2d25a127524a23459bb9d4b4dddca492
Urbain Corentin <corentin.urbain96@gmail.com>
2019-08-25 21:40:15 +0200

    CryGame: Add

---
+++ StarCitizen XML to JSON/CryGame.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml;
+
+namespace StarCitizen_XML_to_JSON
+{
+	public class CryGame
+	{
+		private string filename = "Game.xml";
+		private XmlDocument doc;
+
+		public CryGame(string path)
+		{
+			doc = new XmlDocument();
+			doc.Load(Path.Combine(path, filename));
+		}
+
+		public XmlNode FindRef(string uuid)
+		{
+			return doc.SelectSingleNode($"//*[@__ref=\"{uuid}\"]");
+		}
+
+		public XmlNodeList FindRefs(string uuid)
+		{
+			return doc.SelectNodes($"//*[@__ref=\"{uuid}\"]");
+		}
+	}
+}
