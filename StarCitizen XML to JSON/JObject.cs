212399e665a4022800a5539666079b4728d03d75
Urbain Corentin <corentin.urbain96@gmail.com>
2019-08-25 15:30:32 +0200

    JObject: simplify implementations

--- StarCitizen XML to JSON/JObject.cs
+++ StarCitizen XML to JSON/JObject.cs
@@ -1,7 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Text;
+using System.Text.RegularExpressions;
 using System.Xml;
 
 namespace StarCitizen_XML_to_JSON
@@ -14,7 +15,7 @@ namespace StarCitizen_XML_to_JSON
 
 		public string destination;
 
-		public string directory_name = null;
+		abstract internal string directory_name { get;}
 
 	
 		protected JObject(XmlDocument doc, FileInfo file, string destination)
@@ -25,6 +26,9 @@ namespace StarCitizen_XML_to_JSON
 
 			this.generatedFiles = new List<FileInfo>();
 			doc.Load(file.FullName);
+
+			if (!Directory.Exists(Path.Combine(destination, directory_name)))
+				Directory.CreateDirectory(Path.Combine(destination, directory_name));
 		}
 
 		public abstract void Process();
@@ -40,9 +44,44 @@ namespace StarCitizen_XML_to_JSON
 				{
 					using (StreamReader reader = new StreamReader(file.FullName))
 					{
-						Newtonsoft.Json.JsonConvert.DeserializeObject(reader.ReadToEnd());
+						JsonConvert.DeserializeObject(reader.ReadToEnd());
 					}
 				}
+		}
+
+		/// <summary>
+		/// Write to a JSON file the edited XMLDocument
+		/// </summary>
+		/// <param name="doc">document to write</param>
+		public void WriteFile(XmlDocument doc, string name)
+		{
+			XmlNode root = doc.FirstChild;
+
+			var filename = Path.Combine(
+					destination, directory_name, name.Replace(" ", "_") + ".json");
+
+			using (StreamWriter writer = new StreamWriter(filename))
+			{
+				// serealize XML to JSON
+				var plain_json = JsonConvert.SerializeXmlNode(root, Newtonsoft.Json.Formatting.Indented, true);
+
+				// remove @ before property name
+				plain_json = Regex.Replace(plain_json, "([\"\'])(@)((.*?)[\"\']\\:)", "$1$3");
+
+				// remove bad leading zero (eg: '01.5' or '040')
+				plain_json = Regex.Replace(plain_json, "([\"\'])(0)([0-9\\.]+)([\"\'])", "$1$3$4");
+
+				// add leading 0 on decaml ".x"
+				plain_json = Regex.Replace(plain_json, "([\"\'])(\\.[0-9]+)([\"\'])", "0$2");
+
+				// remove '"' to numbers (int, float, double,..)
+				plain_json = Regex.Replace(plain_json, "([\"\'])([0-9]*[(\\.[0-9]+]?)([\"\'])", "$2");
+
+				writer.Write(plain_json);
+			}
+
+			// add the file to the files list to be validated later
+			generatedFiles.Add(new FileInfo(filename));
 		}
 
 		public string Sanitize(string value)
