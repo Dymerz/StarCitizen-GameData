730a985fc06f9f73859ae15d336760a4f72b6f9b
Urbain Corentin <corentin.urbain96@gmail.com>
2019-08-25 15:31:39 +0200

    JEntitySample: Add

---
+++ StarCitizen XML to JSON/JsonObjects/JEntitySample.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+
+namespace StarCitizen_XML_to_JSON.JsonObjects
+{
+	class JEntitySample : JObject
+	{
+		//public new string directory_name = "Ships";
+		internal override string directory_name { get => "JEntitySample"; }
+
+		public JEntitySample(XmlDocument doc, FileInfo file, string destination) : base(doc, file, destination) { }
+
+		/// <summary>
+		/// Process the XML conversion
+		/// </summary>
+		public override void Process()
+		{
+			var root = doc.SelectSingleNode("/*"); // get the root element
+
+			var name = "JEntitySample";
+			base.WriteFile(doc, name); // write the main ship
+
+			base.ValidateFiles();
+		}
+	}
+}
+
