f449a7e3de4900c14017f9341fad5c8429939433
Urbain Corentin <corentin.urbain96@gmail.com>
2019-08-25 15:31:23 +0200

    JWeapons: Added

---
+++ StarCitizen XML to JSON/JsonObjects/Weapon/JWeapon.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+
+namespace StarCitizen_XML_to_JSON.JsonObjects.Weapon
+{
+	internal class JWeapon : JObject
+	{
+		internal override string directory_name { get => "Weapons"; }
+
+		public JWeapon(XmlDocument doc, FileInfo file, string destination) : base(doc, file, destination) { }
+
+		public override void Process()
+		{
+			var root = doc.SelectSingleNode("/*"); // get the root element
+
+			base.WriteFile(doc, "Sample");
+
+			base.ValidateFiles();
+		}
+	}
+}
