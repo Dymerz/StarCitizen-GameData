df9058b92296e2095cecad8d13f6a74b03ad8558
Urbain Corentin <corentin.urbain96@gmail.com>
2019-08-27 17:01:25 +0200

    CryCacheData: Add

---
+++ StarCitizen XML to JSON/Cry/CryCacheData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Runtime.Serialization;
+using System.Xml;
+
+namespace StarCitizen_XML_to_JSON.Cry
+{
+	[Serializable]
+	class CryCacheData : ISerializable
+	{
+		public string uuid;
+		public XmlNode node;
+
+		public CryCacheData(string uuid, XmlNode node)
+		{
+			this.uuid = uuid ?? throw new ArgumentNullException(nameof(uuid));
+			this.node = node ?? throw new ArgumentNullException(nameof(node));
+		}
+
+		// Deserialize
+		public CryCacheData(SerializationInfo info, StreamingContext context)
+		{
+			uuid = (string)info.GetValue("uuid", typeof(string));
+
+			XmlDocument doc = new XmlDocument();
+			doc.LoadXml((string)info.GetValue("node", typeof(string)));
+			node = doc.SelectSingleNode("/*");
+		}
+
+		// Serialize
+		public void GetObjectData(SerializationInfo info, StreamingContext context)
+		{
+			info.AddValue("uuid", uuid);
+			info.AddValue("node", node.OuterXml);
+		}
+	}
+}
