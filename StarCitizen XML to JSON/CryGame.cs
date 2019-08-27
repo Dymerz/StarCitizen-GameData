43957f52f43d434e2bfe2686aa50e3ebd2306502
Urbain Corentin <corentin.urbain96@gmail.com>
2019-08-27 14:59:02 +0200

    JGame: Optimizing (-586% execution time), add cache

--- StarCitizen XML to JSON/CryGame.cs
+++ StarCitizen XML to JSON/CryGame.cs
@@ -10,6 +10,7 @@ namespace StarCitizen_XML_to_JSON
 	{
 		private string filename = "Game.xml";
 		private XmlDocument doc;
+		private List<Tuple<string, XmlNode>> cache = new List<Tuple<string, XmlNode>>();
 
 		public CryGame(string path)
 		{
@@ -21,14 +22,71 @@ namespace StarCitizen_XML_to_JSON
 		{
 			if (uuid == "00000000-0000-0000-0000-000000000000")
 				return null;
-			return doc.SelectSingleNode($"//*[@__ref=\"{uuid}\"]");
+
+			// Search in Cache
+			var res = GetCache(uuid);
+			if (res != null)
+				return res;
+
+			// Search in Game.xml
+			res = doc.SelectSingleNode($"//*[@__ref=\"{uuid}\"]");
+			AddCache(uuid, res);
+			return res;
 		}
 
-		public XmlNodeList FindRefs(string uuid)
+		public XmlNode[] FindRefs(string uuid)
 		{
 			if (uuid == "00000000-0000-0000-0000-000000000000")
 				return null;
-			return doc.SelectNodes($"//*[@__ref=\"{uuid}\"]");
+			
+			// Search in Cache
+			XmlNode[] res = GetMultipleCache(uuid);
+			if (res != null)
+				return res;
+
+			// Search in Game.xml
+			var nodes = doc.SelectNodes($"//*[@__ref=\"{uuid}\"]");
+			AddMultipleCache(uuid, nodes);
+			if (nodes == null)
+				return null;
+
+			// Convert XMlNodeList to XMlNode[]
+			List<XmlNode> array_nodes = new List<XmlNode>();
+			foreach (XmlNode n in nodes)
+				array_nodes.Add(n);
+			return array_nodes.ToArray();
+		}
+		private void AddCache(string uuid, XmlNode node)
+		{
+			if (!CacheExist(uuid) && node != null)
+				cache.Add(new Tuple<string, XmlNode>(uuid, node));
+		}
+
+		private void AddMultipleCache(string uuid, XmlNodeList nodes)
+		{
+			if (!CacheExist(uuid) && nodes != null)
+			{
+				var enu = nodes.GetEnumerator();
+				while (enu.MoveNext())
+				{
+					cache.Add(new Tuple<string, XmlNode>(uuid, (XmlNode)enu.Current));
+				}
+			}
+		}
+
+		private XmlNode GetCache(string uuid)
+		{
+			return cache.Find(c => c.Item1.Equals(uuid))?.Item2;
+		}
+
+		private XmlNode[] GetMultipleCache(string uuid)
+		{
+			return cache.FindAll(c => c.Item1.Equals(uuid)).ConvertAll(t => t.Item2)?.ToArray();
+		}
+
+		private bool CacheExist(string uuid)
+		{
+			return cache.Exists(c => c.Item1.Equals(uuid));
 		}
 	}
 }
