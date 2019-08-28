70776bed95ff3bcb143d271554fa24b43d5a6c4f
Urbain Corentin <corentin.urbain96@gmail.com>
2019-08-28 15:16:10 +0200

    ProgressBar: Update and bugs fixes

--- StarCitizen XML to JSON/ProgressBar.cs
+++ StarCitizen XML to JSON/ProgressBar.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace StarCitizen_XML_to_JSON
 {
@@ -8,13 +10,15 @@ namespace StarCitizen_XML_to_JSON
 		private IList list;
 
 		private int width = 0;
-		private int height = 0;
 
 		private int length = 0;
 		private int count = 0;
 		private int leading_space = 11;
 
+		private int title_max = 0;
 		private string title;
+
+		private bool auto_title = false;
 		private bool clear_line = false;
 
 		/// <summary>
@@ -23,15 +27,23 @@ namespace StarCitizen_XML_to_JSON
 		/// <param name="list">the object to iterate from.</param>
 		/// <param name="title">A friendly name to print.</param>
 		/// <param name="clear_line">if true, this will clear the line when the process is done.</param>
-		public ProgressBar(IList list, string title = "Progress", bool clear_line = false)
+		public ProgressBar(IList list, string title = "Progress", bool clear_line = false, bool auto_title = false)
 		{
 			this.list = list ?? throw new ArgumentNullException(nameof(ProgressBar.list));
 			this.length = list.Count;
 			this.width = Console.WindowWidth;
-			this.height = Console.WindowHeight;
 
 			this.title = title;
+			this.auto_title = auto_title;
 			this.clear_line = clear_line;
+
+			// if auto_title is true
+			if (auto_title)
+				// get the longest titles object
+				this.title_max = list.Cast<object>().Max(l => l.ToString().Length);
+			else
+				// else, get the length of the title
+				this.title_max = title.Length;
 
 			Refresh();
 		}
@@ -47,16 +59,19 @@ namespace StarCitizen_XML_to_JSON
 			// iterate each object
 			while (en.MoveNext())
 			{
-				yield return en.Current; // return the object to the foreach loop
+				if (auto_title)
+					title = en.Current.ToString();
 
 				// then clean the line with spaces
-				Console.Write('\r' + new String(' ', width) + '\r');
+				Console.Write('\r' + new String(' ', width - 1) + '\r');
 
 				// increment the number of objects already processed
 				count++;
 
 				// Refresh the progress bar
 				Refresh();
+
+				yield return en.Current; // return the object to the foreach loop
 			}
 
 			if (clear_line)
@@ -65,10 +80,11 @@ namespace StarCitizen_XML_to_JSON
 				int cursor_posTop = Console.CursorTop;
 
 				Console.CursorTop = Console.WindowTop + Console.WindowHeight - 1;
-				Console.Write('\r' + new String(' ', width) + '\r');
+				Console.Write('\r' + new String(' ', width - 1) + '\r');
 
 				Console.SetCursorPosition(cursor_posLeft, cursor_posTop);
-			}else
+			}
+			else
 				Console.Write("\r\n");
 		}
 
@@ -82,7 +98,7 @@ namespace StarCitizen_XML_to_JSON
 
 			int percent = (int)((float)count / length * 100); // calculate the progress percentage
 
-			int progress_space = (width - 2) - title.Length - leading_space; // calculate the area of the progress
+			int progress_space = (width - 2) - title_max - leading_space; // calculate the area of the progress
 			int val = (int)(progress_space * ((float)percent / 100)); // calculate the number of '#' to print
 
 			string progress = new String('#', val); // create the progress (###) string
@@ -97,14 +113,11 @@ namespace StarCitizen_XML_to_JSON
 			// place the cursor at the last line
 			Console.CursorTop = Console.WindowTop + Console.WindowHeight - 1;
 
-			// print all the progress info
-			Console.Write($"{title}: [{String.Format("{0,3:##0}", percent)}%] [");
+			// print all progress info (e.g: "Progress: [ 50%] ")
+			Console.Write(String.Format("{0," + title_max + "}: [{1,3:##0}%] ", title, percent));
 
 			// print the loading progress (e.g: ##....)
-			Console.Write(progress + remaining);
-
-			// print the end of the progress bar
-			Console.Write("]");
+			Console.Write($"[{progress}{remaining}]");
 
 			// place the cursor back to his original position
 			Console.SetCursorPosition(cursor_posLeft, cursor_posTop);
