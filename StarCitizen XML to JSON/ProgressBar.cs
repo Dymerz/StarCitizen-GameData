603b19adf9de29339ea58862daa4ade828aa632c
Urbain Corentin <corentin.urbain96@gmail.com>
2019-08-28 13:05:38 +0200

    ProgressBar: Add comments

--- StarCitizen XML to JSON/ProgressBar.cs
+++ StarCitizen XML to JSON/ProgressBar.cs
@@ -17,6 +17,12 @@ namespace StarCitizen_XML_to_JSON
 		private string title;
 		private bool clear_line = false;
 
+		/// <summary>
+		/// Create a loading bar at the bottom of the terminal
+		/// </summary>
+		/// <param name="list">the object to iterate from.</param>
+		/// <param name="title">A friendly name to print.</param>
+		/// <param name="clear_line">if true, this will clear the line when the process is done.</param>
 		public ProgressBar(IList list, string title = "Progress", bool clear_line = false)
 		{
 			this.list = list ?? throw new ArgumentNullException(nameof(ProgressBar.list));
@@ -30,15 +36,26 @@ namespace StarCitizen_XML_to_JSON
 			Refresh();
 		}
 
+		/// <summary>
+		/// Called by the foreach
+		/// </summary>
+		/// <returns></returns>
 		public IEnumerator GetEnumerator()
 		{
-			var en = list.GetEnumerator();
+			var en = list.GetEnumerator(); // get the enumerator of the object
+
+			// iterate each object
 			while (en.MoveNext())
 			{
-				yield return en.Current;
+				yield return en.Current; // return the object to the foreach loop
 
+				// then clean the line with spaces
 				Console.Write('\r' + new String(' ', width) + '\r');
+
+				// increment the number of objects already processed
 				count++;
+
+				// Refresh the progress bar
 				Refresh();
 			}
 
@@ -55,28 +72,41 @@ namespace StarCitizen_XML_to_JSON
 				Console.Write("\r\n");
 		}
 
+		/// <summary>
+		/// Refresh the progress
+		/// </summary>
 		private void Refresh()
 		{
-			int cursor_posLeft = Console.CursorLeft;
-			int cursor_posTop = Console.CursorTop;
+			int cursor_posLeft = Console.CursorLeft; // get the cursor position (left)
+			int cursor_posTop = Console.CursorTop; // get the cursor position (right)
 
-			int percent = (int)((float)count / length * 100);
+			int percent = (int)((float)count / length * 100); // calculate the progress percentage
 
-			int progress_space = (width - 2) - title.Length - leading_space;
-			int val = (int)(progress_space * ((float)percent / 100));
-			string progress = new String('#', val);
-			string remaining = new String('.', progress_space - progress.Length);
+			int progress_space = (width - 2) - title.Length - leading_space; // calculate the area of the progress
+			int val = (int)(progress_space * ((float)percent / 100)); // calculate the number of '#' to print
 
+			string progress = new String('#', val); // create the progress (###) string
+			string remaining = new String('.', progress_space - progress.Length); // create the empty progress string
+
+			// Scrolling
+			// if the cursor is on the progress bar, then place the cursor one line below
+			//	(it will print the progress one line bellow)
 			if (Console.WindowTop + Console.WindowHeight - 1 == Console.CursorTop)
 				Console.CursorTop++;
 
 			// place the cursor at the last line
 			Console.CursorTop = Console.WindowTop + Console.WindowHeight - 1;
 
+			// print all the progress info
 			Console.Write($"{title}: [{String.Format("{0,3:##0}", percent)}%] [");
+
+			// print the loading progress (e.g: ##....)
 			Console.Write(progress + remaining);
+
+			// print the end of the progress bar
 			Console.Write("]");
 
+			// place the cursor back to his original position
 			Console.SetCursorPosition(cursor_posLeft, cursor_posTop);
 		}
 	}
