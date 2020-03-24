c8083ee72feaa58c30bcd26bf59e8903408ffdc0
Urbain Corentin <corentin.urbain96@gmail.com>
2020-03-24 15:57:41 +0100

    build.bat: Check WSL installed before building for Linux

--- StarCitizen JSON to SQL/build.bat
+++ StarCitizen JSON to SQL/build.bat
@@ -3,5 +3,10 @@
 echo Building Win x64
 dotnet publish "StarCitizen JSON to SQL.csproj" -c Release -r win-x64 --self-contained=true
 
-echo Building Linux x64
-wsl dotnet publish "StarCitizen JSON to SQL.csproj" -c Release -r linux-x64 --self-contained=true 
+WHERE wsl
+IF %ERRORLEVEL% EQU 0 (
+	echo Building Linux x64
+	wsl dotnet publish "StarCitizen JSON to SQL.csproj" -c Release -r linux-x64 --self-contained=true
+)else(
+	ECHO Skipping Linux build, missing WSL
+)
